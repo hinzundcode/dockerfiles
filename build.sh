f877390bed8c27e6044b8960762a6debcaf7c57e
Chris Hinze <schokoc+git@appucino.de>
2016-06-25 02:01:29 +0200

    added node v6.2.2

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ docker push hinzundcode/rpi-node:5.11
 docker build --rm --force-rm -t hinzundcode/rpi-node:6.0 rpi-node/6.0
 docker push hinzundcode/rpi-node:6.0
 
-docker tag hinzundcode/rpi-node:6.0 hinzundcode/rpi-node:latest
+docker build --rm --force-rm -t hinzundcode/rpi-node:6.2 rpi-node/6.2
+docker push hinzundcode/rpi-node:6.2
+
+docker tag hinzundcode/rpi-node:6.2 hinzundcode/rpi-node:latest
 docker push hinzundcode/rpi-node:latest
